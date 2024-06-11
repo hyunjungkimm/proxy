2c1040d431a783ef3f09277f2be8f9dbd81caf40
kimhyeonjeong <khjjung1124@gmail.com>
2024-06-11 16:21:15 +0900

    [240611] 동적 프록시 기술 리플렉션, CGLIB

--- src/main/java/hello/proxy/config/v1_proxy/ConcreteProxyConfig.java
+++ src/main/java/hello/proxy/config/v1_proxy/ConcreteProxyConfig.java
@@ -1,15 +1,12 @@
 package hello.proxy.config.v1_proxy;
 
-import hello.proxy.app.v1.*;
+
 import hello.proxy.app.v2.OrderControllerV2;
 import hello.proxy.app.v2.OrderRepositoryV2;
 import hello.proxy.app.v2.OrderServiceV2;
 import hello.proxy.config.v1_proxy.concrete_proxy.OrderControllerConcreteProxy;
 import hello.proxy.config.v1_proxy.concrete_proxy.OrderRepositoryConcreteProxy;
 import hello.proxy.config.v1_proxy.concrete_proxy.OrderServiceConcreteProxy;
-import hello.proxy.config.v1_proxy.interface_proxy.OrderControllerInterfaceProxy;
-import hello.proxy.config.v1_proxy.interface_proxy.OrderRepositoryInterfaceProxy;
-import hello.proxy.config.v1_proxy.interface_proxy.OrderServiceInterfaceProxy;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
